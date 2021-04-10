6a82566b2bc7ee2780f4a69d8960be5a77fbbef2
Tony Foerster <tony@phdata.io>
2021-04-10 18:24:08 -0500

    2019 RDX

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -939,6 +939,7 @@ FW_VERSIONS = {
       b'37805-5YF-A420\x00\x00',
       b'37805-5YF-A430\x00\x00',
       b'37805-5YF-C210\x00\x00',
+      b'37805-5YF-A870\x00\x00',
     ],
     (Ecu.vsa, 0x18da28f1, None): [
       b'57114-TJB-A040\x00\x00',
@@ -956,12 +957,14 @@ FW_VERSIONS = {
     (Ecu.transmission, 0x18da1ef1, None): [
       b'28102-5YK-A700\x00\x00',
       b'28102-5YK-A711\x00\x00',
+      b'28102-5YK-A630\x00\x00',
     ],
     (Ecu.combinationMeter, 0x18da60f1, None): [
       b'78109-TJB-AB10\x00\x00',
       b'78109-TJB-AD10\x00\x00',
       b'78109-TJB-AF10\x00\x00',
       b'78109-TJB-AW10\x00\x00',
+      b'78109-TJB-A240\x00\x00',
     ],
     (Ecu.srs, 0x18da53f1, None): [
       b'77959-TJB-A040\x00\x00',
@@ -974,6 +977,7 @@ FW_VERSIONS = {
     (Ecu.gateway, 0x18daeff1, None): [
       b'38897-TJB-A110\x00\x00',
       b'38897-TJB-A120\x00\x00',
+      b'38897-TJB-A040\x00\x00',
     ],
     (Ecu.eps, 0x18da30f1, None): [
       b'39990-TJB-A030\x00\x00',
